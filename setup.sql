81d8c8dad1f012044f706e5d3a494515d4baad2d
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-08-17 13:02:50 +0100

    Add botcommand table

--- setup.sql
+++ setup.sql
@@ -1,3 +1,12 @@
 create database if not exists neogary;
 create user if not exists 'neogary-dev'@'localhost' identified by 'qwe123*';
 grant all privileges on neogary . * to 'neogary-dev'@'localhost';
+
+-- create tables
+drop table if exists botcommand;
+create table botcommand (
+  id int not null auto_increment,
+  name varchar(20) not null,
+  description varchar(255),
+  primary key (id)
+);
