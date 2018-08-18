ed2b052fe409ab1bbbde7659fa2bbd85742a4626
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-08-18 18:14:34 +0100

    Add role table

--- setup.sql
+++ setup.sql
@@ -3,10 +3,24 @@ create user if not exists 'neogary-dev'@'localhost' identified by 'qwe123*';
 grant all privileges on neogary . * to 'neogary-dev'@'localhost';
 
 -- create tables
+--
+-- botcommand
+--
 drop table if exists botcommand;
 create table botcommand (
   id int not null auto_increment,
   name varchar(20) not null,
   description varchar(255),
+  primary key (id)
+);
+
+--
+-- roles
+--
+drop table if exists role;
+create table role (
+  id int not null auto_increment,
+  discordid varchar(18) not null,
+  isassignable boolean not null,
   primary key (id)
 );
