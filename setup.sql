f4c769f950086e425819a82adca1d5a04590c760
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-08-20 15:55:53 +0100

    Add permtier column

--- setup.sql
+++ setup.sql
@@ -1,26 +1,31 @@
+-- 
+-- create database and user
+--
 create database if not exists neogary;
 create user if not exists 'neogary-dev'@'localhost' identified by 'qwe123*';
 grant all privileges on neogary . * to 'neogary-dev'@'localhost';
 
--- create tables
+-- tables
 --
 -- botcommand
 --
-drop table if exists botcommand;
-create table botcommand (
+drop table if exists neogary.botcommand;
+create table neogary.botcommand (
   id int not null auto_increment,
-  name varchar(20) not null,
+  name varchar(32) not null,
   description varchar(255),
+  permtier int not null,
   primary key (id)
 );
 
 --
--- roles
+-- role
 --
-drop table if exists role;
-create table role (
+drop table if exists neogary.role;
+create table neogary.role (
   id int not null auto_increment,
   discordid varchar(18) not null,
   isassignable boolean not null,
+  permtier int not null,
   primary key (id)
 );
